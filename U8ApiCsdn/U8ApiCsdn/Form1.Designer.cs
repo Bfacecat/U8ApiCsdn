3f59025c2b496676da4c89693039dad22ba7a2ca
wuyaoqing <yaoqingwu@foxmail.com>
2020-10-17 17:00:03 +0800

    提交代码：获取u8登录信息

---
+++ U8ApiCsdn/U8ApiCsdn/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace U8ApiCsdn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetLogin
+            // 
+            this.btnGetLogin.Location = new System.Drawing.Point(24, 12);
+            this.btnGetLogin.Name = "btnGetLogin";
+            this.btnGetLogin.Size = new System.Drawing.Size(146, 42);
+            this.btnGetLogin.TabIndex = 0;
+            this.btnGetLogin.Text = "获取u8登录信息";
+            this.btnGetLogin.UseVisualStyleBackColor = true;
+            this.btnGetLogin.Click += new System.EventHandler(this.btnGetLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 279);
+            this.Controls.Add(this.btnGetLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetLogin;
+    }
+}
+
---
+++ U8ApiCsdn/U8ApiCsdn/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace U8ApiCsdn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 获取u8登录对象
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGetLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sSubId = "ST";
+                string sAccId = "002";
+                string sYear = "2020";
+                string sUserId = "demo";
+                string sPassword = "123456";
+                string sServer = "127.0.0.1";
+                string sSerial = "";
+                string sDate = DateTime.Now.ToString("yyyy-MM-dd");
+                U8Login.clsLogin u8login = GetU8Login(sSubId, sAccId, sYear, sUserId, sPassword, sDate, sServer, sSerial);
+                string DbName = u8login.UfDbName;
+                string UserName = u8login.cUserName;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 构建登录对象
+        /// </summary>
+        /// <param name="sSubId">U8子模块</param>
+        /// <param name="sAccId">账套ID</param>
+        /// <param name="sYear">年份</param>
+        /// <param name="sUserId">用户账号</param>
+        /// <param name="sPassword">用户密码</param>
+        /// <param name="sDate">登陆日期</param>
+        /// <param name="sServer">登陆服务器地址</param>
+        /// <param name="sSerial">默认空</param>
+        /// <returns></returns>
+        public static U8Login.clsLogin GetU8Login(String sSubId, String sAccId, String sYear, String sUserId, String sPassword, String sDate, String sServer, String sSerial)
+        {
+            U8Login.clsLogin U8Login = new U8Login.clsLogin();
+            bool bSucess = false;
+            try
+            {
+                bSucess = U8Login.Login(ref sSubId, ref sAccId, ref sYear, ref sUserId, ref sPassword, ref sDate, ref sServer, ref sSerial);
+                if (bSucess == false)
+                {
+                    String errMsg = "登陆失败,原因:" + U8Login.ShareString;
+                    throw new Exception(errMsg);
+                }
+                else
+                {
+                    return U8Login;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
